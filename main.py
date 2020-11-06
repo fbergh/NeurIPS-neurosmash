4c29eb65e60437af080a2aeb72fe6fe808dad661
fbergh <freekbergh@gmail.com>
2020-11-06 15:14:23 +0100

    Added main.py for running simulations from command-line

---
+++ main.py
@@ -0,0 +1,36 @@
+import argparse
+from neurosmash import Agent, Environment, Episode
+
+
+def main(args):
+    agent = Agent()
+    env = Environment(args.ip, args.port, args.size, args.timescale)
+    episode = Episode(env, agent)
+    n_episodes_won = 0
+
+    for i in range(args.n_episodes):
+        episode.run()
+        if episode.is_win:
+            print(f"Agent won episode {i + 1}")
+            n_episodes_won += 1
+        else:
+            print(f"Agent lost episode {i + 1}")
+
+    print(f"Won/total: {n_episodes_won}/{args.n_episodes}")
+
+
+if __name__ == "__main__":
+    p = argparse.ArgumentParser()
+
+    # Set-up parameters
+    p.add_argument('--ip', type=str, default="127.0.0.1", help="IP address that the TCP/IP interface listens to")
+    p.add_argument('--port', type=int, default=13000, help="Port number that the TCP/IP interface listens to")
+    p.add_argument('--size', type=int, default=96, help="Size of the environment's texture")
+    p.add_argument('--timescale', type=float, default=10.0, help="Simulation speed (higher is faster)")
+
+    # Simulation parameters
+    p.add_argument('--n_episodes', type=int, default=10, help="Number of episodes we want to run")
+
+    args = p.parse_args()
+
+    main(args)
