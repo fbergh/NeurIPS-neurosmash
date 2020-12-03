6b4196ff18e699224bcf35be71640b31356c9bb6
fbergh <freekbergh@gmail.com>
2020-12-03 15:44:55 +0100

    Add contrast stretching to preprocessor

--- preprocessing/preprocessor.py
+++ preprocessing/preprocessor.py
@@ -7,15 +7,16 @@ class Preprocessor:
         img = img / 255
         img = self._reduce_channels(img)
         img = self._crop(img)
+        img = self._stretch_contrast(img)
         return img
 
     def _cleanup_crop_values(self, crop_values):
         left, top, right, bottom = crop_values
-        if bottom == 0: 
+        if bottom == 0:
             bottom = None
-        if right == 0: 
+        if right == 0:
             right = None
-        assert not left ==  right and not  top ==  bottom, "Crop values cannot be equal"
+        assert not left == right and not top == bottom, "Crop values cannot be equal"
         return left, top, right, bottom
 
     def _get_channels_to_keep(self, n_channels):
@@ -34,4 +35,13 @@ class Preprocessor:
     def _crop(self, img):
         img = img[self.top:-self.bottom] if self.bottom else img[self.top:]
         img = img[:, self.left:-self.right] if self.right else img[:, self.left:]
+        return img
+
+    def _stretch_contrast(self, img):
+        """
+        Contrast stretching by rescaling the min and max value of the image to [0,1] per channel
+        """
+        min_max_values = [(img[:, :, c].min(), img[:, :, c].max()) for c in range(img.shape[2])]
+        for channel, (min_val, max_val) in enumerate(min_max_values):
+            img[:, :, channel] = (img[:, :, channel] - min_val) / (max_val - min_val)
         return img
